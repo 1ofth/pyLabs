34b398ad7bd31b519b020133c7a89de14c4456db
KarinaKarin <karina.khusnut999@gmail.com>
2020-10-12 17:10:51 +0300

    added tests

--- test/test_list2.py
+++ test/test_list2.py
@@ -8,10 +8,6 @@ class Test(TestCase):
         b = [2, 5, 6]
         self.assertEqual([1, 2, 3, 4, 5, 6, 7], merge(a, b))
 
-    def test_merge_a_only(self):
-        a = [1, 3, 4, 7]
-        self.assertEqual([1, 2, 3, 4, 5, 6, 7], merge(a, []))
-
-    def test_merge_b_only(self):
-        b = [2, 5, 6]
-        self.assertEqual([1, 2, 3, 4, 5, 6, 7], merge([], b))
+    def test_rm_adj(self):
+        a = [0, 2, 2, 3]
+        self.assertEqual([0, 2, 3], rm_adj(a))
--- test/test_string2.py
+++ test/test_string2.py
@@ -5,3 +5,12 @@ from string2 import *
 class Test(TestCase):
     def test_replace_not_bad(self):
         self.assertEqual('This music is good!', replace_not_bad('This music is not so bad!'))
+
+    def test_v_ing(self):
+        self.assertEqual('reading', v('read'))
+
+    def test_v_ly(self):
+        self.assertEqual('readingly', v('reading'))
+
+    def test_v_short(self):
+        self.assertEqual('eat', v('eat'))
